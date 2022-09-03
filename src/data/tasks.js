ac1afa6e64116fe2683e28003bc874f31821839a
YoannBretonnet <88546350+YoannBretonnet@users.noreply.github.com>
2022-09-03 17:42:32 +0200

    adding components folders and data

---
+++ src/data/tasks.js
@@ -0,0 +1,27 @@
+export default [
+    {
+      id: 1,
+      label: 'Coder une todolist en impératif',
+      done: true,
+      editing: false,
+    },
+    {
+      id: 13,
+      label: 'Coder une todolist en React',
+      done: false,
+      editing: false,
+    },
+    {
+      id: 4,
+      label: 'Appeler Jean-Marc',
+      done: false,
+      editing: false,
+    },
+    {
+      id: 8,
+      label: 'Préparer des crêpes',
+      done: false,
+      editing: false,
+    },
+  ];
+  
